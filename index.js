c2a9433f49a810b275f3aee4888836cdded95667
lmj <mjlee95613@gmail.com>
2024-06-10 22:02:30 +0900

    회원 가입

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
+const bodyParser = require('body-parser');
 const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
-
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! Hi~~')
+})
+
+app.post('/register', async (req, res) => {
+
+    // 회원가입에 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body);
+
+    await user.save()
+        .then(() => {
+            res.status(200).json({
+                success: true
+            })
+        })
+        .catch((err)=>{
+            res.json({ success: false, err })
+        })
+
 })
 
 app.listen(port, () => {
